41c1d5032aea6edc9ac2e16036b6c7a45cfb05d4
pnowakowski614 <p.nowakowski416@gmail.com>
2022-05-09 03:18:58 +0200

    poprawki do logiki, komunikaty,kolory na klawiaturze

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 function playGame() {
-
     const message = document.querySelector(".message-container");
     const wordDisplay = document.querySelector(".word-container");
     const keyboard = document.querySelector(".keyboard-container");
@@ -9,7 +8,9 @@ function playGame() {
         "essence",
         "generator",
         "sample",
-        "text"
+        "acorn",
+        "liabilty",
+        "electric"
     ]
     
     const randInt = Math.floor(Math.random() * wordList.length);
@@ -48,6 +49,9 @@ function playGame() {
     
     let guessesLeft = 6;
     let letterAdded;
+    let lettersGuessed = [];
+
+    const equals = (a, b) => JSON.stringify(a) === JSON.stringify(b);
     
     keys.forEach(key => {
         const buttonElement = document.createElement('button')
@@ -62,21 +66,34 @@ function playGame() {
         letterElement.setAttribute('id', wordLetter)
         wordDisplay.append(letterElement);
     })
+
+    const addColorToKey = (keyLetter, color) => {
+        const coloredKey = document.getElementById(keyLetter);
+        coloredKey.classList.add(color);
+        coloredKey.disabled = true;
+    }
     
     const checkLetter = (addedLetter) => {
+        letterAdded = false;
         for (let i=0; i < passwordTiles.length; i++) {
             if(passwordTiles[i].id.toUpperCase() == addedLetter) {
                 passwordTiles[i].textContent = addedLetter;
+                lettersGuessed.push(passwordTiles[i].id);
                 letterAdded = true;
-            }
-            else {
-                letterAdded = false;
+                addColorToKey(addedLetter, "green-background");
+                if (equals(lettersGuessed.sort(), wordSplit.sort())) {
+                    alert("Congratulations, you've won! :)");
+                }
             }
         }
         if (!letterAdded) {
-            guessesLeft -= 1;
-            message.textContent = `GUESSES LEFT: ${guessesLeft}`
-            if (guessesLeft == 0) alert("You've lost! :( Wanna try again?");
+            if (guessesLeft <= 1) 
+                alert("You've lost! :( Wanna try again?");
+            else {
+                guessesLeft -= 1;
+                addColorToKey(addedLetter, "red-background");
+                message.textContent = `GUESSES LEFT: ${guessesLeft}`;
+            }
         }
     }
 
