4521f9a4530cb6b30cf28c3be04c2746700457b9
pnowakowski614 <p.nowakowski416@gmail.com>
2022-05-27 10:45:32 +0200

    refactor(): refactor the code to TypeScript

--- script.ts
+++ script.ts
@@ -1,8 +1,8 @@
-"use strict";
-const message = document.querySelector(".message-container span");
-const wordDisplay = document.querySelector(".word-container");
-const keyboard = document.querySelector(".keyboard-container");
-const wordList = [
+const message = document.querySelector(".message-container span") as HTMLSpanElement;
+const wordDisplay = document.querySelector(".word-container") as HTMLDivElement;
+const keyboard = document.querySelector(".keyboard-container") as HTMLButtonElement;
+
+const wordList: string[] = [
     "random",
     "words",
     "essence",
@@ -12,7 +12,8 @@ const wordList = [
     "liabilty",
     "electric"
 ];
-const keys = [
+
+const keys: string[] = [
     'Q',
     'W',
     'E',
@@ -40,28 +41,31 @@ const keys = [
     'N',
     'M',
 ];
-let guessesLeft = 6;
-let letterAdded = false;
-const lettersGuessed = [];
-let passwordTiles;
-let word;
-const resetGame = () => {
+
+let guessesLeft: number = 6;
+let letterAdded: boolean = false;
+const lettersGuessed: string[] = [];
+let passwordTiles: HTMLCollection;
+let word: string;
+
+const resetGame = (): void => {
     guessesLeft = 6;
     message.textContent = `${guessesLeft}`;
     letterAdded = false;
     lettersGuessed.splice(0, lettersGuessed.length);
     [...passwordTiles].forEach(tile => tile.remove());
     keys.forEach(key => {
-        const element = document.getElementById(key);
-        element.classList.remove('green-background', 'red-background');
+        const element = document.getElementById(key) as HTMLButtonElement;
+        element.classList.remove('green-background', 'red-background')
         element.disabled = false;
-    });
+    })
     playGame();
-};
-const checkLetter = (addedLetter) => {
+}
+
+const checkLetter = (addedLetter: string): false | undefined => {
     letterAdded = false;
-    for (let i = 0; i < passwordTiles.length; i++) {
-        if (passwordTiles[i].id.toUpperCase() === addedLetter) {
+    for (let i=0; i < passwordTiles.length; i++) {
+        if(passwordTiles[i].id.toUpperCase() === addedLetter) {
             passwordTiles[i].textContent = addedLetter;
             lettersGuessed.push(passwordTiles[i].id);
             letterAdded = true;
@@ -74,8 +78,8 @@ const checkLetter = (addedLetter) => {
             }
         }
     }
-    if (letterAdded)
-        return;
+    if (letterAdded) return;
+
     if (guessesLeft <= 1) {
         alert("You've lost! :( Wanna try again?");
         resetGame();
@@ -86,29 +90,38 @@ const checkLetter = (addedLetter) => {
         addColorToKey(addedLetter, "red-background");
         message.textContent = `${guessesLeft}`;
     }
-};
-const addColorToKey = (keyLetter, color) => {
-    const coloredKey = document.getElementById(keyLetter);
+}
+
+const addColorToKey = (keyLetter: string, color: string): void => {
+    const coloredKey = document.getElementById(keyLetter) as HTMLButtonElement;
     coloredKey.classList.add(color);
     coloredKey.disabled = true;
-};
-const addKeyboardElement = (key) => {
+}
+
+const addKeyboardElement = (key: string): void => {
     const buttonElement = document.createElement('button');
     buttonElement.textContent = key;
     buttonElement.setAttribute('id', key);
     buttonElement.addEventListener('click', () => checkLetter(key));
-    keyboard.append(buttonElement);
-};
-const equals = (a, b) => JSON.stringify(a) === JSON.stringify(b);
-function playGame() {
-    const randInt = Math.floor(Math.random() * wordList.length);
+    keyboard.append(buttonElement)
+}
+
+const equals = (a: string[], b: string[]): boolean => JSON.stringify(a) === JSON.stringify(b);
+
+function playGame(): void {
+    const randInt: number = Math.floor(Math.random() * wordList.length);
     word = wordList[randInt];
+
     word.split('').forEach(wordLetter => {
-        const letterElement = document.createElement('div');
-        letterElement.setAttribute('id', wordLetter);
+        const letterElement = document.createElement('div')
+        letterElement.setAttribute('id', wordLetter)
         wordDisplay.append(letterElement);
     });
+
     passwordTiles = wordDisplay.children;
 }
+
 keys.forEach(key => addKeyboardElement(key));
+
 playGame();
+
